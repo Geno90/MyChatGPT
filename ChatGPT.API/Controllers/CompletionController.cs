238916c4a069da3462da9b79b449ae985f916715
Lucas Bergström <lucas.bergstrom90@gmail.com>
2024-09-26 13:53:41 +0200

    Demo Requset Fixed

--- ChatGPT.API/Controllers/CompletionController.cs
+++ ChatGPT.API/Controllers/CompletionController.cs
@@ -24,7 +24,28 @@ namespace ChatGPT.API.Controllers
         public async Task<ActionResult<string>> SendMessageAsync([FromBody] ChatRequest chatRequest)
         {
 
-            var response = await _completionsService.GetCompletionAsync(chatRequest);
+            var chatRequestDemo = new ChatRequest
+            {
+                Model = "gpt-4o", // Modellen som används, kan vara t.ex. "gpt-3.5-turbo"
+
+                Messages = new List<Message>
+                {
+                      new Message
+                      {
+                          Role = "system",
+                          Content = "Du är glad och välkomnande till den här ChatGpt appen och tacka för att användaren tar sig tid att kolla på Lucas kod. " +
+                          "Detta är ett sidoprojekt som Lucas har byggt för att använda till hans större projekt Diné!. " +
+                          "Han kom på att han antagligen kommer vilja använda mig under flera spännande projekt. Därför bygger han på mig efter behov"
+                      },
+                      new Message
+                      {
+                          Role = "user",
+                          Content = "Hej, vad roligt att vara här! Lucas verkar som en bra kille, ska vi anställa honom?"
+                      },
+                }
+            };
+
+            var response = await _completionsService.GetCompletionAsync(chatRequestDemo);
 
             return response;
         }
